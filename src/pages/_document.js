357701e620b76e5bd19579a3fd4ec28e99c69ec4
zikriramdani <zikriramdani@gmail.com>
2025-02-18 09:04:50 +0700

    CSS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const domain = "https://zrdevelopers.github.io/cara-mudah-wujudkan-semua-kebutuhan";
+
   return (
     <Html lang="en">
       <Head>
-        <link href="./assets/css/bootstrap.css" rel="stylesheet" />
+        <link href={`${domain}/assets/css/bootstrap.css`} rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com/" />
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
-        <link rel="stylesheet" href="./assets/css/fontawesome.all.min.css" />
-        <link rel="stylesheet" href="./assets/css/fontawesome.min.css" />
-        <link rel="stylesheet" href="./assets/css/font-awesome.min.css" />
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+        <link rel="stylesheet" href={`${domain}/assets/css/fontawesome.all.min.css`} />
+        <link rel="stylesheet" href={`${domain}/assets/css/fontawesome.min.css`} />
+        <link rel="stylesheet" href={`${domain}/assets/css/font-awesome.min.css`} />
+        <link rel="stylesheet" type="text/css" href={`${domain}/assets/css/style.css`} />
       </Head>
       <body>
         <Main />
