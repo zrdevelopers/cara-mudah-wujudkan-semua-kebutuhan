26b81421903aafc28e228679fbecb2b46992766e
zikriramdani <zikriramdani@gmail.com>
2025-02-22 11:29:09 +0700

    Content

--- src/pages/api/getAreas/index.js
+++ src/pages/api/getAreas/index.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
+      // Kirim request ke API eksternal
       const domainApi = process.env.NEXT_PUBLIC_DOMAIN_API;
-      const response = await fetch(domainApi + '/Api/getAreas', {
+      const response = await fetch(`${domainApi}/Api/getAreas`, {
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json'
-        }
+        },
       });
 
       if (!response.ok) {
@@ -14,11 +16,13 @@ export default async function handler(req, res) {
 
       const data = await response.json();
       return res.status(200).json(data);
+
     } catch (error) {
       return res.status(500).json({ message: 'Error fetching data', error: error.message });
     }
   } else {
-    res.setHeader('Allow', ['GET']);
+    res.setHeader('Allow', ['POST']);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
